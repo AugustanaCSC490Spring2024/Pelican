4470c5aefc386fd088581620caa3c98239d0f57e
Joon <joonsoolee18@augustana.edu>
2024-02-29 08:55:50 -0600

    first page of the app UI

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { Button, Alert, StyleSheet, Text, View } from 'react-native';
+
+const setTitle = () => {
+  setTitleText("Welcome!");
+}
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Pelican Marketplace App</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.name}>
+        Campus
+        {"\n"}
+        Marketplace
+        {"\n"}
+        App
+      </Text>
+      <Button style={styles.button} onPress={()=>Alert.alert('You tapped the button!')} title="Tap to Begin"/>
     </View>
   );
 }
@@ -14,7 +26,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'left',
     justifyContent: 'center',
+    paddingLeft: 30
   },
+  name: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    marginBottom: 50
+  },
+  button: {
+    // alignItems: 'center',
+    // textAlign: 'center',
+    // padding: 10,
+    // borderRadius: 10,
+  }
 });
