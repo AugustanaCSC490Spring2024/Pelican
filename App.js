0c8e699463ed00bf9806af7aec01c2374ebbef40
Stephanie Nhi Le <ledaquynhnhi@gmail.com>
2024-04-12 17:17:06 -0500

    refactor

--- App.js
+++ App.js
@@ -16,75 +16,13 @@ import {AuthContainer} from './services/authContainer';
 import { useNavigation } from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
-// const navigationRef = React.useRef(null);
+
 GoogleSignin.configure({
   webClientId: "354512485882-kl4hbo3936mu38lm19k8sq4pnvg54jm4.apps.googleusercontent.com",
 });
 
   export default function App() {
-    // const user = authContainer()
-    // const { user, initializing} = AuthContainer();
-    // const router = useRouter();
-    // const navigation = useNavigation();
-    // const navigationState = useRootNavigationState();
-
-    // if (initializing) {
-    //   return <Text>Loading...</Text>;
-    // }
-
-    
     return (
-      // <NavigationContainer>
-      //   {user ? (
-      //     <Stack.Navigator>
-      //       <Stack.Screen name="Home" component={Home} options={{ headerShown: true }}/>
-      //       <Stack.Screen name="Tab" component={TabLayout} options={{ headerShown: true }} />
-      //       {/* Other screens when user is logged in */}
-      //     </Stack.Navigator>
-      //   ) : (
-      //     <Stack.Navigator>
-      //       <Stack.Screen name="AuthContainer" component={AuthContainer} options={{ headerShown: true }}/>
-      //       {/* Other screens when user is not logged in */}
-      //     </Stack.Navigator>
-      //   )}
-      // </NavigationContainer>
-    // );
-
-    // if (!user) {
-    //   console.log('redirecting to login');
-    //   // router.replace('./auth');
-    //   // return <Redirect href="./authContainer"/>;
-    //   navigation.navigate('AuthContainer');
-    // } else if (user) {
-    //   console.log('redirect to home');
-    //   // router.replace('../app/(tabs)/Home');
-    //   // return <Redirect href="../app/(tabs)/Home"  />;
-    //   navigation.navigate('Home');
-    // }
-
-    // return <NavigationContainer>{!navigationState?.key ? <Text>Loading...</Text> : <></>}</NavigationContainer>;
-
-    // return (
-    // <SafeAreaProvider>
-    //   <AuthContainer />
-    // </SafeAreaProvider>
-
-  // <NavigationContainer>
-  //   <AuthContainer />
-  // </NavigationContainer>
-
-    // <NavigationContainer>
-    // <Stack.Navigator initialRouteName="Auth">
-    //   <Stack.Screen name="Auth" component={AuthContainer} options={{ headerShown: true }} />
-    //   <Stack.Screen name="Main" component={BottomTab} options={{ headerShown: true }} />
-    // </Stack.Navigator>
-    // </NavigationContainer>
-
-    // <NavigationContainer>
-    //   {user ? <Home /> : <AuthContainer />}
-    //   {/* <AuthContainer /> */}
-    // </NavigationContainer>
-
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Auth" component={AuthContainer} options={{ headerShown: true }} />
