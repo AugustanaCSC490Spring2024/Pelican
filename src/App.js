6825f37c0f891d81b33c7cc3f732823c6f610cb6
Stephanie Nhi Le <ledaquynhnhi@gmail.com>
2024-04-18 11:10:37 -0500

    refactor & use remote database

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import logo from './icon.png';
 import './App.css';
-import React, {  useState } from 'react';
+import React from 'react';
+import ProductView from './component/Products/productView';
 
 function App() {
-
-  let [products, setName] = React.useState(
-    ['POLO Sweater', 'iPhone X', 'Galaxy S9', 'Used Textbook', 'Coffee Machine', 'Easter Egg', 'Nuke']
-  );
-
-  let [price, setPrice] = React.useState(
-    ['$15', '$200', '$220', '$20', '$22', 'Free', '$1,000,000']
-  )
-
-  let [sellers, setSeller] = React.useState(
-    ['jslee402', 'XanderBender', 'SobanTuban', 'StephanieCurry', 'Stonedahl101', 'Rockdahl102', 'NorthKorea']
-  )
-
-  let copyPrice = [...price]
-  let copySeller = [...sellers]
-
   return (
     <div className="App">
       <div className='navbar'>
@@ -29,28 +13,7 @@ function App() {
           <li className='link-item post-btn'><a href=''>Post</a></li>
         </ul>
       </div>
-
-      {/* <button onClick={ ()=> {
-        let copy = [...price];
-        copy.sort();
-        setPrice(copy);
-      } }>Sort by Price</button> */}
-
-      {
-        products.map(function(prd,i){
-          return (
-            <div className='post'> 
-              <img src={logo} className='prd-img'/>
-              <div className='prd-info'>
-                <h5>{ prd }</h5>
-                <p>{ copySeller[i] }</p>
-                <p>{ copyPrice[i] }</p>
-              </div>
-            </div>
-          )
-        })
-      }
-
+      <ProductView/>
     </div>
   );
 }
--- src/component/Products/productView.js
+++ src/component/Products/productView.js
@@ -1,18 +1,46 @@
 import useProducts from "./useProducts";
+import logo from '../../assets/icon.png'
+import React, { StyleSheet } from 'react';
+// import { StyleSheet } from 'react-native';
+
 
 const ProductView = () => {
     const products = useProducts();
     return (
         <div>
             {products.map((product) => (
-                <div key={product.key}>
-                    <h2>{product.name}</h2>
-                    <p>{product.user}</p>
-                    <p>${Number(product.price).toLocaleString()}</p>
+                <div key={product.key} style={styles.post}>
+                    <img alt="img" src={logo} style={styles.prdImg}/>
+                    <div style={styles.prdInfo}>
+                        <h2 style={styles.prdInfoH5}>{product.name}</h2>
+                        <p>{product.user}</p>
+                        <p>${Number(product.price).toLocaleString()}</p>
+                    </div>
                 </div>
             ))}
         </div>
     );
 }
+
+const styles = {
+    /* Post Style */
+    post: {
+        display: 'grid',
+        gridTemplateColumns: '1fr 4fr',
+        marginBottom: '5px',
+        marginTop: '5px',
+    },
+    prdImg: {
+        width: '100%',
+    },
+    prdInfo: {
+        paddingLeft: '15px',
+        paddingTop: '15px',
+    },
+    prdInfoH5: {
+        fontSize: '2em',
+        margin: '0',
+    },
+};
 
 export default ProductView;
