ebf2053d1b0ed01145ae099800064ab5960f9cd7
Joon <joonsoolee18@augustana.edu>
2024-04-18 08:58:14 -0500

    recommit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import logo from './icon.png';
 import './App.css';
-import React from 'react';
-import ProductView from './component/Products/productView';
+import React, {  useState } from 'react';
 
 function App() {
+
+  let [products, setName] = React.useState(
+    ['POLO Sweater', 'iPhone X', 'Galaxy S9', 'Used Textbook', 'Coffee Machine', 'Easter Egg', 'Nuke']
+  );
+
+  let [price, setPrice] = React.useState(
+    ['$15', '$200', '$220', '$20', '$22', 'Free', '$1,000,000']
+  )
+
+  let [sellers, setSeller] = React.useState(
+    ['jslee402', 'XanderBender', 'SobanTuban', 'StephanieCurry', 'Stonedahl101', 'Rockdahl102', 'NorthKorea']
+  )
+
+  let copyPrice = [...price]
+  let copySeller = [...sellers]
+
   return (
     <div className="App">
       <div className='navbar'>
