f06cdfd320ac86aac01d07f0ecb1ccd0dae5d351
Joon <joonsoolee18@augustana.edu>
2024-04-18 08:53:07 -0500

    developed main UIs

--- src/App.js
+++ src/App.js
@@ -23,12 +23,25 @@ function App() {
     <div className="App">
       <div className='navbar'>
         <h2>Pelican Marketplace</h2>
+        <ul className='nav-links'>
+          <li className='link-item'><a href=''>Home</a></li>
+          <li className='link-item'><a href=''>Chat</a></li>
+          <li className='link-item post-btn'><a href=''>Post</a></li>
+        </ul>
       </div>
+
+      {/* <button onClick={ ()=> {
+        let copy = [...price];
+        copy.sort();
+        setPrice(copy);
+      } }>Sort by Price</button> */}
+
       {
         products.map(function(prd,i){
           return (
-            <div>
-              <div>
+            <div className='post'> 
+              <img src={logo} className='prd-img'/>
+              <div className='prd-info'>
                 <h5>{ prd }</h5>
                 <p>{ copySeller[i] }</p>
                 <p>{ copyPrice[i] }</p>
@@ -37,6 +50,7 @@ function App() {
           )
         })
       }
+
     </div>
   );
 }
