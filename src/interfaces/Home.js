cfb7182222b48b709d938673554bfe0e14767370
Joon <joonsoolee18@augustana.edu>
2024-04-24 19:41:39 -0500

    Post Page Style Update

--- src/interfaces/Home.js
+++ src/interfaces/Home.js
@@ -7,32 +7,8 @@ import ProductView from '../component/Products/productView';
 
 const Home = () => {
 
-
-  let [products, setName] = React.useState(
-    ['POLO Sweater', 'iPhone X', 'Galaxy S9', 'Used Textbook', 'Coffee Machine', 'Easter Egg', 'Nuke']
-  );
-
-  let [price, setPrice] = React.useState(
-    ['$15', '$200', '$220', '$20', '$22', 'Free', '$1,000,000']
-  )
-
-  let [sellers, setSeller] = React.useState(
-    ['jslee402', 'XanderBender', 'SobanTuban', 'StephanieCurry', 'Stonedahl101', 'Rockdahl102', 'NorthKorea']
-  )
-
-  let copyPrice = [...price]
-  let copySeller = [...sellers]
-
     return (
         <div className="App">
-            {/* <div className='navbar'>
-                <h2>Pelican Marketplace</h2>
-                <ul className='nav-links'>
-                    <li className='link-item'><a href='/'>Home</a></li>
-                    <li className='link-item'><a href='/Chat'>Chat</a></li>
-                    <li className='link-item post-btn'><a href='/Post'>Post</a></li>
-                </ul>
-            </div> */}
             <Navigation></Navigation>
             <ProductView></ProductView>
 
