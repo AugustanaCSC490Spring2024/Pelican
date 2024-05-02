e78f07dee608ec638e3b074f0cbf55d127a8f797
Soban <sobanhassan111@gmail.com>
2024-05-02 01:16:25 -0500

    Added signout button to the home page so user can logout. Added the functionality so only augustana.edu emails can sign in. No other users are allowed anymore

--- src/interfaces/Navigation.js
+++ src/interfaces/Navigation.js
@@ -3,12 +3,11 @@ import React, { useEffect, useState } from 'react';
 import logoTrans from '../assets/logo-trans.png';
 import { db, auth } from '../data/firebase.js'
 import SearchComponent from '../component/Search/SearchBar.js';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
 
 const Navigation = () => {
-  // const [results, setResults] = useState([]);
   const [showForm, setShowForm] = useState(false);
   const [user, setUser] = useState(null);
 
@@ -28,6 +27,15 @@ const Navigation = () => {
     return () => unsubscribe();
   }, [navigate]);
 
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate('/');
+    } catch (error) {
+      console.error('Logout error:', error);
+    }
+  };
+
   if (!user) {
     return null;
   }
@@ -38,21 +46,22 @@ const Navigation = () => {
   };
 
   return (
-      <div className='navbar'>
-        <h3>Welcome, {user.displayName}!</h3>
-          <div style={{display: 'flex', alignItems: 'center'}}>
-            <img src={logoTrans} alt='logo' style={{width: '70px', height: '70px'}}></img>
-            <h2 style={{ fontWeight: 'bold' }}>Pelican Marketplace</h2>
-          </div>
-        <div style={styles.box}>
-          <ul className='nav-links'>
-            <li className='link-item'><SearchComponent db={ db } /></li>
-            <li className='link-item'><a href='/home'>Home</a></li>
-            <li className='link-item'><a href='/chat'>Chat</a></li>
-            <li className='link-item post-btn'><a href='/post'>Post</a></li>
-          </ul>          
-        </div>
-        {/* <div className = "search-bar-container">
+    <div className='navbar'>
+      <h3>Welcome, {user.displayName}!</h3>
+      <button className='link-item post-btn' onClick={handleLogout}>Logout</button>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={logoTrans} alt='logo' style={{ width: '70px', height: '70px' }}></img>
+        <h2 style={{ fontWeight: 'bold' }}>Pelican Marketplace</h2>
+      </div>
+      <div style={styles.box}>
+        <ul className='nav-links'>
+          <li className='link-item'><SearchComponent db={db} /></li>
+          <li className='link-item'><a href='/home'>Home</a></li>
+          <li className='link-item'><a href='/chat'>Chat</a></li>
+          <li className='link-item post-btn'><a href='/post'>Post</a></li>
+        </ul>
+      </div>
+      {/* <div className = "search-bar-container">
           <SearchComponent db={ db } />
         </div>
         <ul className='nav-links'>
@@ -60,8 +69,8 @@ const Navigation = () => {
           <li className='link-item'><a href='/chat'>Chat</a></li>
           <li className='link-item post-btn'><a href='/post'>Post</a></li>
         </ul> */}
-      
-      </div>
+
+    </div>
   );
 }
 
@@ -72,9 +81,9 @@ const styles = {
   }
 }
 
-{/* <ProductView/> */}
-{/* <AddProductForm /> */}
-{/* Put here temporarily - will modify where to put later */}
-{/* {showForm ? <AddProductForm /> : <ProductView />} */}
+{/* <ProductView/> */ }
+{/* <AddProductForm /> */ }
+{/* Put here temporarily - will modify where to put later */ }
+{/* {showForm ? <AddProductForm /> : <ProductView />} */ }
 
 export default Navigation;
